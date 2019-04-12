136a8e13540e6a4f02cc6972c1ace932bf01f0c4
niels <niels@DESKTOP-4IUFEVF>
2019-04-12 21:07:20 +0200

    correctet alorithm

--- src/com/qualitype/de/LeapYear/CalculateLeapYear.java
+++ src/com/qualitype/de/LeapYear/CalculateLeapYear.java
@@ -7,19 +7,19 @@ public class CalculateLeapYear {
 
 	public static void main(String[] args) {
 		List<Integer> leapYears = new ArrayList<Integer>();
-		for (int i = 1804; i < 2401; i++) {
+		for (int i = 1800; i < 2401; i++) {
 			leapYears.add(i);
 		}
 		for (Integer integer : leapYears) {
-			System.out.println(integer + " :" + isLeapYear(integer));
+			System.out.println(integer + " : " + isLeapYear(integer));
 		}
 
 	}
 	
 	private static boolean isLeapYear(int year) {
-		if (year % 4 == 0) return true;
 		if(year % 400 == 0) return true;
 		if(year % 100 == 0) return false;
+		if (year % 4 == 0) return true;
 		
 		return false;
 	}
